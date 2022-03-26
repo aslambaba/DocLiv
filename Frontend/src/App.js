fadc93e78cb0ee04e27b360c5fcaa06ee0190020
sharjeelyunus <sharjeel924@gmail.com>
2022-03-26 18:50:16 +0500

    📦 NEW: About

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { Header } from './components/Header/Header';
+import HomePage from './components/HomePage/HomePage';
 
 function App() {
   return (
     <div className='app'>
       <Header />
+      <HomePage />
     </div>
   );
 }
